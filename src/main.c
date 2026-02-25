557cd481d37ba37c42140164b4a4e3e9b6bb9de6
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-25 09:08:47 +0530

    Sketch out bare minimum structures and functions

--- src/main.c
+++ src/main.c
@@ -9,9 +9,35 @@
 #define WIDTH 900
 #define HEIGHT 600
 #define FPS 60
+#define NBOIDS 30
+
+
+// --- STRUCTURES ------------>
+
+typedef struct s_vec2
+{
+	float x, y;
+} Vec2;
+
+typedef struct s_boid
+{
+	Vec2 position, velocity;
+	float radius;
+} Boid;
+
+
+// --- PROTOTYPES ------------>
+
+void InitBoids(void);
+void Separation(void);
+void Alignment(void);
+void Cohesion(void);
+void DrawBoids(void);
 
 
 // --- ENTRY POINT ------------>
+
+Boid boids[NBOIDS];
 
 int main(void)
 {
@@ -22,10 +48,45 @@ int main(void)
     {
 		BeginDrawing();
 			ClearBackground(BLACK);
+			InitBoids();
+
+			Separation();
+			Alignment();
+			Cohesion();
+
+			DrawBoids();
 		EndDrawing();
     }
     
     CloseWindow();
 
 	return 0;
+}
+
+
+// --- IMPLEMENTATIONS ------------>
+
+void InitBoids(void)
+{
+	return;
+}
+
+void Separation(void)
+{
+	return;
+}
+
+void Alignment(void)
+{
+	return;
+}
+
+void Cohesion(void)
+{
+	return;
+}
+
+void DrawBoids(void)
+{
+	return;
 }
