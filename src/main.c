ab257b54c389abba2aa049517a36da06f84c3af3
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-25 09:02:21 +0530

    Basic raylib setup

--- src/main.c
+++ src/main.c
@@ -1,8 +1,31 @@
-#include <stdio.h>
+#include <raylib.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+// --- CONSTANTS ------------>
+
+#define WIDTH 900
+#define HEIGHT 600
+#define FPS 60
+
+
+// --- ENTRY POINT ------------>
 
 int main(void)
 {
-	printf("Hello World!\n");
+	InitWindow(WIDTH, HEIGHT, "Boids");
+    SetTargetFPS(FPS);
+
+	while (!WindowShouldClose())
+    {
+		BeginDrawing();
+			ClearBackground(BLACK);
+		EndDrawing();
+    }
+    
+    CloseWindow();
+
 	return 0;
 }
